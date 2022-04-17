d4e6b169eaa493ade20859ce2416bcaede2b4722
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2022-04-17 15:25:34 +0200

    Add images with carrierwave, S3, nad figro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_09_120456) do
+ActiveRecord::Schema.define(version: 2022_04_17_075925) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_04_09_120456) do
     t.string "website"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
