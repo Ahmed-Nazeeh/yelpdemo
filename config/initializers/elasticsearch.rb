d61f939b9d474ab6bcaa62e9e2db21a9bc69ae3c
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2022-05-13 15:35:59 +0200

    fix bug heroku searchbox env var

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -8,6 +8,13 @@ client = Elasticsearch::Client.new hosts: [
       scheme: 'https'
     } ]
 
+if Rails.env == 'production'
+    url = ENV["SEARCHBOX_URL"]
+    transport_options = { request: { timeout: 250 } }
+    options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
+    Searchkick.client = Elasticsearch::Client.new(options)
+end
+
     # Searchkick.client = Elasticsearch::Client.new(hosts: [
     #     "localhost:9200"], retry_on_failure: true, 
     #     transport_options: {request: {timeout: 250}})
